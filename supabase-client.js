32075a60c9f7daa461ea3eebc5529962c4aea26f
niarlean-ln <niarlean@gmail.com>
2026-09-11 09:56:43 +0700

    Add files via upload

---
+++ supabase-client.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://ojxuctswzothzvcvdqtl.supabase.co/rest/v1/';
+const SUPABASE_KEY = 'sb_publishable_L3dN-E7PxIehQw6XX1IzdQ_NiKgJTNQ';
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
